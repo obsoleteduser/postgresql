7ede7f704e7b7cd89a61f4334a4d90ec0c95e4ef
tahirdibirovresearch <dibirovtahir@gmail.com>
2022-11-26 21:17:35 +0400

    DB Done

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -6,25 +6,32 @@ class UserController{
         const {name, surname} = req.body
         console.log(name, surname)
         const newPerson = await db.query(`INSERT INTO person (name, surname) values ($1, $2) RETURNING *`, [name, surname])
-        res.json(newPerson)
+        res.json(newPerson.rows[0])
 
     }
 
-    async getUser(req, res){
-
+    async getUsers(req, res){
+        const users  = await db.query(`SELECT * FROM person`)
+        res.json(users.rows)
     }
 
     async getOneUser(req, res){
-
+        const id = req.params.id
+        const user = await db.query(`SELECT * FROM person WHERE id = $1`, [id])
+        res.json(user.rows[0])
     }
 
     async updateUser(req, res){
-
+        const {id, name, surname} = req.body
+        const user = await db.query(`UPDATE person SET name = $1, surname = $2 WHERE id = $3 RETURNING *`, [name, surname, id] )
+        res.json(user.rows[0])
     }
 
 
     async deleteUser(req, res){
-
+        const id = req.params.id;
+        const user =  await db.query(`DELETE FROM person WHERE id = $1`, [id])
+        res.json(user.rows[0])
     }
 }
 
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,7 +4,7 @@ const router = new Router()
 const userController = require('../controllers/user.controller')
 
 router.post('/user', userController.createUser)
-router.get('/user', userController.getUser)
+router.get('/user', userController.getUsers)
 router.get('/user/:id', userController.getOneUser)
 router.put('/user', userController.updateUser)
 router.delete('/user/:id', userController.deleteUser)
